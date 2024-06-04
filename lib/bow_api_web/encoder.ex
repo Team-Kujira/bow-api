fb504fc7486138a9e6988209ad39b79ae1fa52a1
codehans <94654388+codehans@users.noreply.github.com>
2024-06-04 13:22:22 +0100

    add pair

--- lib/bow_api_web/encoder.ex
+++ lib/bow_api_web/encoder.ex
@@ -1,10 +1,40 @@
 require Protocol
 
+defimpl Jason.Encoder, for: Tuple do
+  alias Kujira.Fin.Pair
+
+  def encode({Pair, address}, opts) do
+    {:ok, pair} = Kujira.Fin.get_pair(BowApi.Node.channel(), address)
+    Jason.Encode.map(pair, opts)
+  end
+
+  def encode({k, v}, opts) do
+    Jason.Encode.map(%{k => v}, opts)
+  end
+end
+
+Protocol.derive(Jason.Encoder, Kujira.Fin.Pair,
+  only: [
+    :address,
+    :owner,
+    :token_base,
+    :token_quote,
+    :price_precision,
+    :decimal_delta,
+    :is_bootstrapping,
+    :fee_taker,
+    :fee_maker
+    # :book
+  ]
+)
+
+Protocol.derive(Jason.Encoder, Kujira.Fin.Book, only: [:asks, :bids])
+
 Protocol.derive(Jason.Encoder, Kujira.Bow.Pool.Stable,
   only: [
     :address,
     :owner,
-    # :fin_pair,
+    :fin_pair,
     :token_lp,
     :token_base,
     :token_quote,
@@ -33,7 +63,7 @@ Protocol.derive(Jason.Encoder, Kujira.Bow.Pool.Xyk,
   only: [
     :address,
     :owner,
-    # :fin_pair,
+    :fin_pair,
     :token_lp,
     :token_base,
     :token_quote,
@@ -49,7 +79,7 @@ Protocol.derive(Jason.Encoder, Kujira.Bow.Pool.Lsd,
   only: [
     :address,
     :owner,
-    # :fin_pair,
+    :fin_pair,
     :token_lp,
     :token_base,
     :token_quote,
